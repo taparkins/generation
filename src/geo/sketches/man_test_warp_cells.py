5cbd692d5389213f4c9adabce3ccb633fb188a12
Talia Parkinson <talia.a.parkinson@gmail.com>
2026-06-12 22:02:39 -0600

    Shift some manual testing files into src/geo/sketches
    These test scripts are bulky and interesting enough to warrant saving
    for future reference, but I definitely want to keep them isolated so
    it's clear they are their own thing. This commit adds a "sketches"
    module where they can live, and I can run them if I ever defcide to.
    
    Instructions:
     - Start up venv
     - pip install -e .
     - python3 src/geo/sketches/<NAME OF SCRIPT>

---
+++ src/geo/sketches/man_test_warp_cells.py
@@ -0,0 +1,49 @@
+from src.geo.warp_cells import WarpedCells
+from src.geo.point import Point
+from src.geo.line import Line
+from src.raster.scene import RasterScene
+from src.raster.point import RasterPoint
+from src.raster.line import RasterLine
+
+import random
+import string
+
+scene_size = (800, 800)
+margin = 100
+artboard_size = (
+    scene_size[0] - (2 * margin),
+    scene_size[1] - (2 * margin),
+)
+
+wc = WarpedCells(
+    artboard_size,
+    (20, 20),
+    5, 10,
+    3, 6,
+)
+
+scene = RasterScene(scene_size[0], scene_size[1])
+
+def _make_point_model(pt):
+    return RasterPoint(
+        pt + Point(margin, margin),
+        2,
+    )
+
+def _make_line_model(l):
+    return RasterLine(
+        l.translated(margin, margin),
+        '#99a3f0ff',
+    )
+
+for l in wc.lines:
+    scene.add_model_object(_make_line_model(l))
+for pt in wc.warped_lattice.lattice:
+    scene.add_model_object(_make_point_model(pt))
+
+def _gen_filepath():
+    suffix = ''.join(random.sample(string.ascii_letters, 8))
+    return (f'output/test_{suffix}.png')
+
+scene.draw()
+scene.save(_gen_filepath())
